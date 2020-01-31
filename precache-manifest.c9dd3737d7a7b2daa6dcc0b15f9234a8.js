7c1bd94ab01894facf2a4599f60a91d92ee08c94
Alex Nazarenko <alexnaz8@gmail.com>
2020-01-31 15:20:48 +0200

    Updates

---
+++ precache-manifest.c9dd3737d7a7b2daa6dcc0b15f9234a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57430fc5fe92f1b10ca497570d1461cc",
+    "url": "/firebase-saga-auth/index.html"
+  },
+  {
+    "revision": "e034f3d0ec58395454a2",
+    "url": "/firebase-saga-auth/static/js/2.b2047942.chunk.js"
+  },
+  {
+    "revision": "f007e5fde3f2c3983c1b0d874ba90212",
+    "url": "/firebase-saga-auth/static/js/2.b2047942.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99fda80a24f7c382ff26",
+    "url": "/firebase-saga-auth/static/js/main.0e844fca.chunk.js"
+  },
+  {
+    "revision": "7aa56503a71d3d82954a",
+    "url": "/firebase-saga-auth/static/js/runtime-main.9e4a0a28.js"
+  }
+]);
